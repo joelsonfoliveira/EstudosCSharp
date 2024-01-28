195a6dd1be1da3fd242342059cb94a4b9689a8f6
JOELSON FREITAS DE OLIVEIRA <joelsonfoliveira@gmail.com>
2024-01-28 10:25:42 -0300

    Saída de dados formatada em C#

--- PrimeiroProjeto/PrimeiroProjeto/Program.cs
+++ PrimeiroProjeto/PrimeiroProjeto/Program.cs
@@ -4,6 +4,7 @@ public class Program
 {
     public static void Main(string[] args)
     {
+        
         int idade = 32;
         string nome = "Rafael";
         double saldo = 10.35784;
@@ -12,6 +13,25 @@ public class Program
         Console.WriteLine($"{nome} tem {idade} anos e tem saldo igual a {saldo:F2} reais"); //Interpolação
         Console.WriteLine(nome + " tem " + idade + " anos e tem saldo igual a "
             + saldo.ToString("F2", CultureInfo.InvariantCulture) + " reais"); //Concatenação
-                
+        
+
+        string produto1 = "Computador";
+        string produto2 = "Mesa de escritório";
+
+        byte idade2 = 30;
+        int codigo = 5290;
+        char genero = 'M';
+
+        double preco1 = 2100.00;
+        double preco2 = 650.50;
+        double medida = 53.234567;
+
+        Console.WriteLine("\nProdutos:");
+        Console.WriteLine("{0}, cujo preço é $ {1:F2}", produto1, preco1);
+        Console.WriteLine("{0}, cujo preço é $ {1:F2}", produto2, preco2);
+        Console.WriteLine($"\nRegistro: {idade2} anos de idade, código {codigo} e gênero: {genero}");
+        Console.WriteLine($"\nMedida com oito casas decimais: {medida:F8}");
+        Console.WriteLine($"Arredondado (três casas decimais): {medida:F3}");
+        Console.WriteLine("Separador decimal invariant culture: " + medida.ToString("F3", CultureInfo.InvariantCulture));
     }
 }
