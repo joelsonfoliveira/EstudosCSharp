fe6e98a6f394574dbcd0de8baf282d446119e003
JOELSON FREITAS DE OLIVEIRA <joelsonfoliveira@gmail.com>
2024-01-27 23:27:27 -0300

    Placeholder, Interpolação e Concatenação

---
+++ PrimeiroProjeto/PrimeiroProjeto/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int idade = 32;
+        string nome = "Rafael";
+        double saldo = 10.35784;
+
+        Console.WriteLine("{0} tem {1} anos e tem saldo igual a {2:F2} reais", nome, idade, saldo); //Placeholder
+        Console.WriteLine($"{nome} tem {idade} anos e tem saldo igual a {saldo:F2} reais"); //Interpolação
+        Console.WriteLine(nome + " tem " + idade + " anos e tem saldo igual a "
+            + saldo.ToString("F2", CultureInfo.InvariantCulture) + " reais"); //Concatenação
+                
+    }
+}
